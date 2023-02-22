d0e0d66c37013508fa5f5c3fb8840ea13b00247d
BracketCove <bracket.cove@gmail.com>
2023-02-22 15:47:40 -0700

    FEATURE Google maps initialization stuff

---
+++ iosApp/iosApp/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  iosApp
+//
+//  Created by Ryan Kay on 2023-02-22.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import GoogleMaps
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+       let key = Bundle.main.object(forInfoDictionaryKey: "MAPS_API_KEY")
+        
+        if  let configKey = key as? String {
+            GMSServices.provideAPIKey(configKey)
+            print("Key retrieved")
+        }   else {
+            print("Error: could not retrieve maps api key")
+        }
+        return true
+    }
+}
